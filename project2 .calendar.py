ad7981477a497f302a5d0080a0151bc3be78fba4
KMONIKA26 <139866342+KMONIKA26@users.noreply.github.com>
2023-08-02 15:04:22 +0530

    Add files via upload

---
+++ project2 .calendar.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkcalendar import *
+
+# create object
+root = Tk()
+
+# set geometry
+root.geometry("400x400")
+
+#add calendar
+cal = Calendar(root, selectmode = 'day',year = 2020, month =5,day = 22)
+cal.pack(pady=20)
+
+def grad_date():
+    date.config(text = "Selected Date is: "+ cal.get_date())
+    
+# add button and label 
+Button(root, text = "Get Date",command = grad_date).pack(pady=20)
+date = Label(root, text= "")
+date.pack(pady = 20)
+#Execute tkinter
+root.mainloop()
